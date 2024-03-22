ee383eb9ef25b9f054dd7fb96a3bc5f7f1968370
Ash-art-arch <120031662+Ash-art-arch@users.noreply.github.com>
2024-03-23 02:31:05 +0530

    Second update

---
+++ client/src/pages/Dashboard.jsx
@@ -0,0 +1,63 @@
+import React from 'react'
+import "./dashboard.css"
+import spend from "../assets/spend.svg"
+import visit from "../assets/visit.svg"
+const Dashboard = () => {
+  return (
+    <div className='dashboard'>
+        <div className='dashboard-nav'>
+            <div className='top'>
+               <i class="ri-menu-line"></i>
+                <div className='pfp'></div>
+            </div>
+            <div className='middle'>
+                <a href="">Dashboard</a>
+                <a href="">Loans</a>
+                <a href="">Add Items</a>
+                <a href="">Get Logistic Support</a>
+            </div>
+            <div className='bottom'>
+                <button> Logout</button>
+            </div>
+        </div>
+        <div className='dashboard-content'>
+            <h1>
+                DashBoard
+            </h1>
+            <div className='upper'>
+                <div className='chart'>
+
+                </div>
+                <div className='tally'>
+                    <div className='spend'>
+                        <section className='earn'> 
+                            <h6>Total Earnings</h6>
+                            <p>2000000 USD</p>
+                        </section >
+                        <section className="lo" style={{ backgroundImage: `url(${spend})` }}> 
+
+                        </section>
+                    </div>
+                    <div className='spend'>
+                    <section className='earn'> 
+                            <h6>Person Visited</h6>
+                            <p>345 Impresseions</p>
+                        </section >
+                        <section className="lo" style={{ backgroundImage: `url(${visit})` }}> 
+
+                        </section>
+                    </div>
+                    <div></div>
+                </div>
+            </div>
+            <div className='lower'>
+                <div className='news'>
+                    <h1>Todays News</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Dashboard
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,14 +1,40 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import regbg from "../assets/regbg.svg"
+import { useState } from 'react'
 import "./register.css"
 import { Link } from 'react-router-dom'
 const Register = () => {
       // Get the current URL
 var currentURL = window.location.href;
 
+var role = currentURL.split("?role=")[1];
+console.log(role)
 // Replace 'login' with 'register'
 var newURL = currentURL.replace('/register', '/login');
+const [name,setName]=useState("")
+const [email,setEmail]=useState("")
+const [password,setPassword]=useState("")
+// const [role,setRole]=useState(temp_role)
+
+const handleSubmit= async (e)=>{
+  e.preventDefault()
+  console.log(name,email,password)
+  const response = await fetch('http://localhost:3000/register', {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+          name,
+          email,
+          password,
+          role
+      })
+  })
+  console.log(response)
+}
+
   return (
     <div className='register' >
         <Navbar/>
@@ -18,11 +44,10 @@ var newURL = currentURL.replace('/register', '/login');
             <div className='register-box'>
                 <h1>Register</h1>
                 <div className='form'>
-                    <form >
-                        <input type="text" placeholder='Name' />
-                        <input type="email" placeholder='Email' />
-                        <input type="password" placeholder='Password' />
-                        <input type="password" placeholder='Confirm Password' />
+                    <form onSubmit={(e)=>handleSubmit(e)}>
+                        <input type="text" placeholder='Name'value={name} onChange={(e)=>setName(e.target.value)} />
+                        <input type="email" placeholder='Email' value={email} onChange={(e)=>setEmail(e.target.value)} />
+                        <input type="password" placeholder='Password' value={password} onChange={(e)=>setPassword(e.target.value)}/>
                         <button>Register</button>
                         <p>Already have an account? <Link to={newURL} className='login-a'>Login</Link></p>
                     </form>
