75f777978640d5ff80fe88948816bb2c918a590f
KingrogKDR <abhisheksaikia766@gmail.com>
2024-03-23 09:42:59 +0530

    minor db fix

--- server/src/db/db.js
+++ server/src/db/db.js
@@ -3,7 +3,7 @@ import { DB_NAME } from "../constants.js";
 
 const connectDB = async() => {
     try {
-        const connectedInstance = await mongoose.connect(`mongodb://0.0.0.0:27017/${DB_NAME}`)
+        const connectedInstance = await mongoose.connect(`${process.env.DB_URI}/${DB_NAME}`)
         console.log(`Mongo DB connected!! Hosted on: ${connectedInstance.connection.host}`);
     } catch (error) {
         console.log("Error: ", error?.message || "MongoDB failed to connect")
