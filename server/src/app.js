311e7816d198aff2bb5d8da190bb964733f794c1
Ash-art-arch <120031662+Ash-art-arch@users.noreply.github.com>
2024-03-23 10:25:46 +0530

    Third update

--- server/src/app.js
+++ server/src/app.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import { userRouter } from "./routes/user.routes.js";
 import { orderRouter } from "./routes/order.routes.js";
+import { productRouter } from "./routes/product.routes.js"
 
 const app = express();
 
@@ -20,23 +21,18 @@ app.use(cookieParser());
 // Routes
 app.use("/users", userRouter);
 app.use("/orders", orderRouter);
+app.use("/products", productRouter)
 
 // Error handling middleware
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).send('Something broke!');
 });
-app.use(express.urlencoded({
-    extended: true,
-    limit : "16kb"
-}))
 
 
-import { productRouter } from "./routes/product.routes.js"
 
-app.use("/users", userRouter)
-app.use("/orders", orderRouter)
-app.use("/products", productRouter)
+
+
 
 export default app
 
